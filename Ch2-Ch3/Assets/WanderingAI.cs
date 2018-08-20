73c4f6435b61bfe4daa2f4b8322995833297ac90
Anastasia Shatova <a_tasja@mail.ru>
2018-08-20 13:34:40 +0300

    20/8/2018-1

--- Ch2-Ch3/Assets/WanderingAI.cs
+++ Ch2-Ch3/Assets/WanderingAI.cs
@@ -21,26 +21,32 @@ public class WanderingAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         // enemy постоянно движется вперед если жив
-        if(_alive) 
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        //луч науливается туда куда движется цель
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-        //бросаем луч с описанной вокруг него окружностью
-        if(Physics.SphereCast(ray, 0.75f, out hit))
+        if (_alive)
         {
-            
-            GameObject hitObject = hit.transform.gameObject;
-            if (hitObject.GetComponent<PlayerCharacter>())
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            //луч нацеливается туда куда движется цель
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            //бросаем луч с описанной вокруг него окружностью
+            if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                _fireball = Instantiate(fireballPrefab) as GameObject;
-                //поместим огненный шар перед врагом и нацелим его в направлении его движения
-                _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
-                _fireball.transform.rotation = transform.rotation;
+
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    //поместим огненный шар перед врагом и нацелим его в направлении его движения
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+
+                else if (hit.distance < obstracleRange)
+                {
+                    float angle = Random.Range(-110, 100); //поворачиваем в наполовину случайном направлении
+                    transform.Rotate(0, angle, 0);
+
+                }
             }
-        }else if(hit.distance < obstracleRange) {
-            float angle = Random.Range(-110, 100); //поворачиваем в наполовину случайном направлении
-            transform.Rotate(0, angle, 0);
         }
     }
 
