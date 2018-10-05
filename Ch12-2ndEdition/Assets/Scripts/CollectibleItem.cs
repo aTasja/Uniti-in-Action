80bc235ed10883412990291fcb00e42d2c4ec68a
Anastasiya Shatova <a_tasja@mail.ru>
2018-10-05 12:19:35 +0300

    5-10-2018 sounds completed

--- Ch12-2ndEdition/Assets/Scripts/CollectibleItem.cs
+++ Ch12-2ndEdition/Assets/Scripts/CollectibleItem.cs
@@ -6,13 +6,17 @@ public class CollectibleItem : MonoBehaviour {
 
     [SerializeField] private string itemName;
 
-	void OnTriggerEnter(Collider other)
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip grabSound;
+
+    void OnTriggerEnter(Collider other)
     {
         Debug.Log("Item collected: " + itemName);
         //только игрок может собирать русурсы, но если у enemy убрать компнент rigid body, 
         //нужный для попаданая снарядов в него, то проверять коллаидер на то что он игрока не нужно
         if (other.gameObject.name == "player") 
         {
+            audioSource.PlayOneShot(grabSound);
             Managers.Inventory.AddItem(itemName);
             Destroy(this.gameObject);
         }
